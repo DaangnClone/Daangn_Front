16693dab86823362ac88ba8bb04817d343651ab7
yongchane <202021113@sangmyung.kr>
2024-08-28 00:26:40 +0900

    #5 footer 작업중
    앱스토어 구글 플레이 스토어 에셋 다운 작업 필요 추가 하단 작업 필요

--- daangnclone/src/layout/Footer.jsx
+++ daangnclone/src/layout/Footer.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+// import { ReactComponent as Daangnlogo } from "../assets/applogo/";
+// import { ReactComponent as Daangnlogo } from "../assets/logo/daangnlogo.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <div className="footer-container-line" />
+      <div className="footer-content-container">
+        <div className="footer-content-container-one">
+          <div className="footer-content-one-one">중고거래</div>
+          <div className="footer-content-one-two">동네업체</div>
+          <div className="footer-content-one-three">당근알바</div>
+          <div className="footer-content-one-four">부동산</div>
+          <div className="footer-content-one-five">중고차직거래</div>
+        </div>
+        <div className="footer-content-container-two">
+          <div className="footer-content-two-one">당근비즈니스</div>
+          <div className="footer-content-two-two">채팅하기</div>
+        </div>
+        <div className="footer-content-container-third">
+          <div className="footer-content-third-one">자주 묻는 질문</div>
+          <div className="footer-content-third-two">회사 소개</div>
+          <div className="footer-content-third-third">인재 채용</div>
+        </div>
+        <div className="footer-content-container-app">
+          <div className="footer-content-container-app-title">
+            당근 앱 다운로드
+          </div>
+          <div className="footer-content-container-app-icon">
+            <div className="footer-content-app-container">앱스토어</div>
+            <div className="footer-content-app-container"> 구글</div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Footer;
